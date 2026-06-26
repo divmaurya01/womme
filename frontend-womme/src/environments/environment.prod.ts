08ddb8f44c8dfb6a22302b0e3c32d3efa6bf8fb7
Nowara InfoTech <divmaurya02@gmail.com>
2026-06-26 12:55:41 +0530

    prod push
    prod push

--- frontend-womme/src/environments/environment.prod.ts
+++ frontend-womme/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://192.168.6.27:5100/api',
-  fileBaseUrl: 'http://192.168.6.27:5100'
+  apiBaseUrl: 'http://manhourapp.dyndns.org:5100/api',
+  fileBaseUrl: 'http://manhourapp.dyndns.org:5100'
+  // apiBaseUrl: 'http://192.168.6.27:5100/api',
+  // fileBaseUrl: 'http://192.168.6.27:5100'
 };
